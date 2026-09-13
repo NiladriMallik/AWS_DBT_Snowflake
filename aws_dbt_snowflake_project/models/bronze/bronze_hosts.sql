bb2521f3084e61143349f705ef4daf1906c34ace
niladri.mallik007@gmail.com <51795733+NiladriMallik@users.noreply.github.com>
2026-09-13 10:43:36 +0530

    Code for explanation of incremental model

--- aws_dbt_snowflake_project/models/bronze/bronze_hosts.sql
+++ aws_dbt_snowflake_project/models/bronze/bronze_hosts.sql
@@ -1,1 +1,9 @@
+{% set incremental_flag = 1 %}
+{% set incremental_column = 'CREATED_AT' %}
+
 SELECT * FROM {{ source('staging', 'hosts') }}
+{% if incremental_flag == 1 %}
+    WHERE {{ incremental_column }} > (
+        SELECT COALESCE(MAX({{ incremental_column }}), '1900-01-01') FROM {{ this }}
+    )
+{% endif %}
