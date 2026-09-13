d379c741d62ffb46823d5e43c18cbe11a5478f89
niladri.mallik007@gmail.com <51795733+NiladriMallik@users.noreply.github.com>
2026-09-13 22:31:39 +0530

    Add silver models and macros for incremental processing in dbt project

---
+++ aws_dbt_snowflake_project/models/silver/silver_bookings.sql
@@ -0,0 +1,18 @@
+{{
+  config(
+    materialized = 'incremental',
+    unique_key='BOOKING_ID',
+    on_schema_change = 'sync_all_columns'
+    )
+}}
+
+SELECT
+    BOOKING_ID,
+    LISTING_ID,
+    BOOKING_DATE,
+    {{ multiply('NIGHTS_BOOKED', 'BOOKING_AMOUNT', 2) }} AS TOTAL_AMOUNT,
+    CLEANING_FEE,
+    SERVICE_FEE,
+    BOOKING_STATUS,
+    CREATED_AT
+FROM {{ ref('bronze_bookings') }}
